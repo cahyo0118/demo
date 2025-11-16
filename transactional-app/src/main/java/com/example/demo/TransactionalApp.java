4ad4847e79a65e000e8dd141ed881f6bfdef9e45
Nur Cahyo Putro <nurcahyo@ideku.id>
2025-11-17 01:04:56 +0700

    [transactional] add user POJO, and add package for mysql jpa

---
+++ transactional-app/src/main/java/com/example/demo/TransactionalApp.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalApp {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(TransactionalApp.class, args);
+    }
+}
