c5589ffc40d8d93b24afd9d96028e1e5031aae76
Nur Cahyo Putro <nurcahyo@ideku.id>
2025-11-24 01:20:37 +0700

    [transactional] add logs to track missing optimistic exception

--- transactional-app/src/main/java/com/example/demo/service/ProductService.java
+++ transactional-app/src/main/java/com/example/demo/service/ProductService.java
@@ -2,7 +2,9 @@ package com.example.demo.service;
 
 import com.example.demo.model.Product;
 import com.example.demo.repository.ProductRepository;
+import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,15 +12,18 @@ import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductService {
 
     private final ProductRepository repo;
 
     @Transactional
     public void updateStock(Long id, BigDecimal newStock, int delayMs) {
+        Gson gson = new Gson();
 
         Product product = repo.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("product not found"));
+        log.info("[{}] original product: {}", newStock, gson.toJson(product));
 
         try {
             Thread.sleep(delayMs);
@@ -26,5 +31,12 @@ public class ProductService {
         }
 
         product.setStock(newStock);
+        repo.saveAndFlush(product);
+
+        log.info("[{}] saveAndFlush stock: {}", newStock, gson.toJson(product));
+
+        Product result = repo.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("product not found"));
+        log.info("[{}] Updated stock: {}", newStock, gson.toJson(result));
     }
 }
